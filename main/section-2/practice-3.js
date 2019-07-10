979314b2708d25d294f9df3e0bde8117ec6d1295
Teng YANG <teng.yang@oocl.com>
2019-07-10 21:23:26 +0800

    Implement all practices

--- main/section-2/practice-3.js
+++ main/section-2/practice-3.js
@@ -1,5 +1,28 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  const result = {};
+  const keys = [];
+  const pushItem = (key, value) => {
+    if (!keys.includes(key)) {
+      keys.push(key);
+      result[key] = parseInt(value);
+    } else {
+      result[key] = result[key] + parseInt(value);
+    }
+  };
+  const splitKeys = ['-', ':', '['];
+  const matchSplitKey = str => {
+    const matchKeys = splitKeys.filter(item => str.includes(item));
+    return matchKeys[0]
+  };
+  collection.forEach(item => {
+    const matchKey = matchSplitKey(item);
+    if (!matchKey) {
+      pushItem(item, 1);
+    } else {
+      pushItem(...item.split(matchKey));
+    }
+  });
+  return keys.map(item => ({ name: item, summary: result[item] }));
 }
